95659cb0106f63668638eb84f37b45641263941c
Edyta <ulanowicz.edyta@gmail.com>
2024-03-20 10:25:29 +0100

    feat: pokemon carousel - fetch pokemons sprites

--- server/src/typeDefs.ts
+++ server/src/typeDefs.ts
@@ -1,45 +1,42 @@
-import gql from "graphql-tag" 
- 
- const typeDefs = gql`
- type Query {
-     "Query to get pokemon specified by id"
-     getPokemon(id: Int!): Pokemon!
+import gql from 'graphql-tag';
 
-     "Query to get pokemon array for the homepage carousel (paginated)"
-     getPokemons(limit: Int!, offset: Int!): ResponsePaginationPokemon
- }
+const typeDefs = gql`
+    type Query {
+        "Query to get pokemon specified by id"
+        getPokemon(id: Int!): Pokemon!
 
- type Pokemon {
-    id: Int!
-    name: String!
-    weight: Int
-    base_experience: Int
-    height:Int
-    is_default: Boolean!
-    order: Int
-    pokemon_species_id: Int
-    sprites: PokemonSprites
-}
+        "Query to get pokemon array for the homepage carousel (paginated)"
+        getPokemons(limit: Int!, offset: Int!): ResponsePaginationPokemon
+    }
 
-type PokemonSprites {
-    front_default: String
-    front_shiny: String
-}
+    type Pokemon {
+        id: Int!
+        name: String!
+        weight: Int
+        base_experience: Int
+        height: Int
+        is_default: Boolean!
+        order: Int
+        pokemon_species_id: Int
+        sprites: PokemonSprites
+    }
 
- type ResponsePaginationPokemon{
-    count: Int 
-    next: String
-    previous: String
-    results:[NamedAPIResource]
- }
+    type PokemonSprites {
+        front_default: String
+        front_shiny: String
+    }
 
- type NamedAPIResource{
-    name: String
-    url: String
- }
- 
- 
+    type ResponsePaginationPokemon {
+        count: Int
+        next: String
+        previous: String
+        results: [NamedAPIResource]
+    }
 
-`
+    type NamedAPIResource {
+        name: String
+        url: String
+    }
+`;
 
- export default typeDefs;
+export default typeDefs;
