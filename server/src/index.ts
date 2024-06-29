8ba021d6b64ba1a423a0984f4cae23d6187c2a4d
Agnieszka26 <medrek.agn@yahoo.com>
2024-06-29 12:51:21 +0200

    feat: add cusom db from mongodb

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,41 @@
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import {startStandaloneServer } from '@apollo/server/standalone';
 import typeDefs from './typeDefs';
 import resolvers from './resolvers';
 import PokemonAPI from './datasource/pokemon-api';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+  const uri =  "mongodb+srv://aga:haslo123@pokemonv2.afijphe.mongodb.net/?retryWrites=true&w=majority&appName=pokemonv2";
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
   });
-  
+ const server = new ApolloServer({ typeDefs, resolvers });
 
+  async function connectToDatabase() {
+    try {
+      await client.connect();
+      try {
+       const db = client.db("aga");
+       const coll = db.collection("ids_pokemon");
+       const cursor = coll.find();
+       await cursor.forEach(console.log);
+      } catch (error) {
+        console.log(error)
+      } finally{
+        await client.close();
+      }
+
+      console.log('Db Connected');
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+  connectToDatabase();
+  
   const { url } = await startStandaloneServer(server, {
     context: async () => {
       const { cache } = server;
