18983282b4e0f3057f6424bbac5eccd5625cdbe3
agnieszkaM <agnieszka.medrek@reikiholding.com>
2024-08-19 16:45:30 +0200

    fix: exp

--- server/api/index.ts
+++ server/api/index.ts
@@ -8,10 +8,87 @@ import cors from "cors";
 import http from "http";
 import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-import typeDefs from "../src/typeDefs";
-import resolvers from "../src/resolvers";
+import gql from "graphql-tag";
 import PokemonAPI from "../src/datasource/pokemon-api";
 import PokemonMongo from "../src/datasource/mongo-api";
+// import typeDefs from "../src/typeDefs";
+// import resolvers from "../src/resolvers";
+// import PokemonAPI from "../src/datasource/pokemon-api";
+// import PokemonMongo from "../src/datasource/mongo-api";
+const resolvers = {
+    Query: {
+      // returns a base details of pokemons
+      getPokemon: (__, { id }, { dataSources }) => {
+        return  dataSources.pokemonAPI.getPokemon(id);
+      },
+      // returns a paginated array of pokemons that will be used to populate the homepage carousel of our web client
+      getLimitedPokemons: (__, { limit, offset }, { dataSources }) => {
+        return dataSources.pokemonAPI.getLimitedPokemons(limit, offset);
+      },
+      //pokemons from custom mongodb
+      pokemons: async (__, _, { dataSources }) => {
+        return await dataSources.basePokemons.pokemons();
+      },
+      //pokemon by its id from custom mongodb
+      pokemon: async (__, { pokemonId }, { dataSources }) => {
+        return await dataSources.basePokemons.pokemon(pokemonId);
+      },
+    },
+    NamedAPIResource: {
+      url: ({ url }, _, { dataSources }) => {
+        return dataSources.pokemonAPI.getPokemonByUrl(url);
+      },
+    },
+  };
+const typeDefs = gql`
+  type Query {
+    "Query to get pokemon specified by id"
+
+    getPokemon(id: Int!): Pokemon!
+
+    "pokemon from our db"
+    pokemons: [BasePokemon]
+    pokemon(id: Int): BasePokemon
+    "Query to get pokemon array for the homepage carousel (paginated)"
+    getLimitedPokemons(limit: Int!, offset: Int!): ResponsePaginationPokemon
+  }
+
+  type ResponsePaginationPokemon {
+    count: Int
+    next: String
+    previous: String
+    results: [NamedAPIResource!]!
+  }
+
+  type NamedAPIResource {
+    name: String!
+    url: Pokemon!
+  }
+
+  type Pokemon {
+    id: Int!
+    name: String!
+    weight: Int
+    base_experience: Int
+    height: Int
+    is_default: Boolean!
+    order: Int
+    pokemon_species_id: Int
+    sprites: PokemonSprites!
+  }
+
+  type PokemonSprites {
+    front_default: String
+    front_shiny: String
+  }
+
+  "BasePokemon object"
+  type BasePokemon {
+    id: String
+    name: String
+  }
+
+`;
 
 dotenv.config();
 
